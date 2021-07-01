0a8ede90b8a900759ba4c5ed57898943517a751a
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2021-07-01 14:14:13 +0000

    feat(mikado): preparation for Mikado tests

---
+++ src/js/psm-p-m_common.js
@@ -0,0 +1,14 @@
+export const constants = {
+  URL: 'wss://node-ws-message-broker.herokuapp.com/',
+  APP_ROOT: document.querySelector('[data-app]'),
+  NUMBERS: {
+    time: {
+      min: 8,
+      max: 256
+    },
+    values: {
+      min: 0,
+      max: 255
+    }
+  }
+}
